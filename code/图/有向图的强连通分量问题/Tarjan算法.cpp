8a91103e8fc08bf3e3436b5737ad22030514bd0a
日沉云起 <csjiangfeng1212@163.com>
2019-02-25 20:45:55 +0800

    Create Tarjan算法.cpp

---
+++ code/图/有向图的强连通分量问题/Tarjan算法.cpp
@@ -0,0 +1,29 @@
+vector<int>graph[MAX];
+//index[i]表示i是第几个被访问的结点,lowLink[i]表示从i出发经有向边可到达的所有节点中最小的index,sccno[i]表示i所属的强连通分量的编号
+int index[MAX],lowLink[MAX],sccno[MAX],dfsNo=0,scc_cnt=0;
+stack<int>s;
+void DFS(int v){
+    index[v]=lowLink[v]=++dfsNo;
+    s.push(v);
+    for(int i:graph[v]){
+        if(index[i]==0){
+            DFS(i);
+            lowLink[v]=min(lowLink[v],lowLink[i]);
+        }else if(sccno[i]==0)
+            lowLink[v]=min(lowLink[v],index[i]);
+    }
+    if(lowLink[v]==index[v]){//是一个强连通分支的根结点
+        ++scc_cnt;
+        int t;
+        do{
+            t=s.top();
+            s.pop();
+            sccno[t]=scc_cnt;
+        }while(t!=v);
+    }
+}
+void Tarjan(){
+    for(int i=1;i<=n;++i)//假设结点由1~n编号
+        if(index[i]==0)
+            DFS(i);
+}
