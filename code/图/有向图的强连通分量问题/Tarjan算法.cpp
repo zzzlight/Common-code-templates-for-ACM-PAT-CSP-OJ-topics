0415435e3b4eaff50cd11c72b46f854f92773e68
日沉云起 <csjiangfeng1212@163.com>
2019-03-29 17:09:41 +0800

    Update Tarjan算法.cpp

--- code/图/有向图的强连通分量问题/Tarjan算法.cpp
+++ code/图/有向图的强连通分量问题/Tarjan算法.cpp
@@ -1,7 +1,7 @@
 vector<int>graph[MAX];
 //index[i]表示i是第几个被访问的结点,lowLink[i]表示从i出发经有向边可到达的所有节点中最小的index,sccno[i]表示i所属的强连通分量的编号
 int index[MAX],lowLink[MAX],sccno[MAX],dfsNo=0,scc_cnt=0;
-stack<int>s;
+stack<int>s;//算法结束时s为空
 void DFS(int v){
     index[v]=lowLink[v]=++dfsNo;
     s.push(v);
