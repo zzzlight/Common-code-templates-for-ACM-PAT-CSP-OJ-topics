d6860edd4c02e346c62b394047c773b347eb2128
日沉云起 <csjiangfeng1212@163.com>
2019-05-17 12:30:22 +0800

    Update 欧拉筛法求解n以内的素数表.cpp

--- code/数学/素数/欧拉筛法求解n以内的素数表.cpp
+++ code/数学/素数/欧拉筛法求解n以内的素数表.cpp
@@ -1,4 +1,4 @@
-//以下代码求解`[2,n)`以内的素数表（注意不包含数字`n`）
+//以下代码求解[2,n)以内的素数表（注意不包含数字n）
 vector<int>prime;//素数表存储在prime中，prime是全局变量
 void findPrime(int n){
     vector<bool>f(n,true);//i为素数，则f[i]=true;i不是素数，则f[i]=false
